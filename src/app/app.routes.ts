78f95c7102c9ba12b89acf135595dfe54a216fb7
vijay <gadgetzone@gadgetzones-MacBook-Air-2.local>
2025-08-11 13:08:29 +0530

    we are live

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    {
-        path:'',
-        component:HomeComponent
+    // {
+    //     path:'',
+    //     component:HomeComponent
 
-    },
-    {
-        path:"home", component:HomeComponent
-    },
+    // },
+    // {
+    //     path:"home", component:HomeComponent
+    // },
 ];
